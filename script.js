8295de93ad7308195d5e454852a87ef8b3fa3279
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-09-01 13:40:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-def fibonacci(n):
-    # Base cases
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 0
-    
-    # Initialize the Fibonacci sequence
-    fib = [0] * (n + 1)
-    fib[1] = 1
-    
-    # Calculate Fibonacci numbers
-    for i in range(2, n + 1):
-        fib[i] = fib[i - 1] + fib[i - 2]
-    
-    return fib[n - 1]
 
-# Test cases
-print(fibonacci(1))  # Output: 0
-print(fibonacci(5))  # Output: 3
+function fibonacci(arr,n) {
 
-
-module.exports = fibonacci;
+	let a = -1
+    let b = 1
+    let c = 0
+    for(let i=0; i<arr.length; i++)
+    {
+        c = a+b
+        if(i==n)
+        {
+            return c
+        }
+        a = b
+        b = c
+    }
+	return 0
+}
