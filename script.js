19d2cc73f447bbcde33063c2eff801b81c4f27cf
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-09-01 13:42:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-function fibonacci(arr,n) {
+function fibonacci(n) {
 
 	let a = -1
     let b = 1
     let c = 0
-    for(let i=0; i<arr.length; i++)
+    for(let i=0; i<=n; i++)
     {
         c = a+b
         if(i==n)
